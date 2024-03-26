6e786a47c8e85a45f6ade7798c35236190e4290f
TranAnhTin <144192851+TranAnhTin@users.noreply.github.com>
2024-03-26 15:11:28 +0700

    push lần 1

---
+++ TranAnhTin_QLSV/src/main/java/thiGK/ntu63132691/TranAnhTinQlsvApplication.java
@@ -0,0 +1,13 @@
+package thiGK.ntu63132691;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranAnhTinQlsvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranAnhTinQlsvApplication.class, args);
+	}
+
+}
---
+++ TranAnhTin_QLSV/src/test/java/thiGK/ntu63132691/TranAnhTinQlsvApplicationTests.java
@@ -0,0 +1,13 @@
+package thiGK.ntu63132691;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranAnhTinQlsvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
