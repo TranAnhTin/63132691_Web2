67224fef2e82fb5e6d8eebfa47719cbcb8fdec15
TranAnhTin <144192851+TranAnhTin@users.noreply.github.com>
2024-04-16 15:15:01 +0700

    ck

---
+++ Project_CK/project_ck/src/main/java/tin/ta/project_ck/ProjectCkApplication.java
@@ -0,0 +1,13 @@
+package tin.ta.project_ck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectCkApplication.class, args);
+	}
+
+}
---
+++ Project_CK/project_ck/src/test/java/tin/ta/project_ck/ProjectCkApplicationTests.java
@@ -0,0 +1,13 @@
+package tin.ta.project_ck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
