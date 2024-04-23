8e709c0d9be5ec1ae5f5d685bfc15c16503da060
TranAnhTin <144192851+TranAnhTin@users.noreply.github.com>
2024-04-23 16:07:13 +0700

    push

---
+++ Project_CK/project_ck/src/main/java/tin/ta/project_ck/Controllers/HomeController.java
@@ -0,0 +1,19 @@
+package tin.ta.project_ck.Controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import tin.ta.project_ck.Models.StudentModel;
+@Controller
+@RequestMapping("/students")
+public class HomeController {
+	@GetMapping("/all")
+	public String getAll(Model model) {
+		model.addAttribute("dsSV", dsSV);
+		return "dsSV";
+	}
+}
