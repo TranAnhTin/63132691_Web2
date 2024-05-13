5bf3b3c6b8bf8ecdd0d4d737ae927b9e51c0b084
TranAnhTin <144192851+TranAnhTin@users.noreply.github.com>
2024-05-13 19:11:13 +0700

    push

--- Project_CK/project_ck/src/main/java/tin/ta/project_ck/Controllers/HomeController.java
+++ Project_CK/project_ck/src/main/java/tin/ta/project_ck/Controllers/HomeController.java
@@ -8,11 +8,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import tin.ta.project_ck.Models.StudentModel;
+import tin.ta.project_ck.Repo.SinhVienRepo;
 @Controller
+
 @RequestMapping("/students")
 public class HomeController {
-	@GetMapping("/all")
+	@Autowired
+    private SinhVienRepo sinhVienRepository;
+	@GetMapping("/")
 	public String getAll(Model model) {
-		return "index";
+		Iterable<StudentModel> students = sinhVienRepository.findAll();
+        model.addAttribute("students", students);
+        return "index";
 	}
 }
